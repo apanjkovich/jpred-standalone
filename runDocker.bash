b12952c48f4360572afcb4bdf778d77a37d53677
Sasha <sasha.panjkovich@gmail.com>
2020-01-21 20:46:24 +0100

    modified mount points to work locally on repo directory

--- runDocker.bash
+++ runDocker.bash
@@ -1,2 +1,2 @@
-docker run -d -it -v /home/apanjkovich/extdata/blastdb:/home/docker_user/mounted/blastdb -v /home/apanjkovich/fsync/Docker/jpredDocker/jpred:/home/docker_user/jpred --user docker_user:10000 -e UID=$(id -u) --name  jpredcontainer2  jpredimage2:latest
+docker run -d -it -v $(pwd)/data:/home/docker_user/data -v $(pwd)/io:/home/docker_user/io -v $(pwd)/jpred:/home/docker_user/jpred --user docker_user:10000 -e UID=$(id -u) --name  jpredcontainer2  jpredimage2:latest
 
