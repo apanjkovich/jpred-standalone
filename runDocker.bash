397ebaee93d7470700abd5ca8934381644302728
Alfredo Luque <aluque@celgene.com>
2020-01-21 18:26:11 +0100

    Use HOME variable and delete personal fsync

--- runDocker.bash
+++ runDocker.bash
@@ -1,2 +1,2 @@
-docker run -d -it -v /home/apanjkovich/extdata/blastdb:/home/docker_user/mounted/blastdb -v /home/apanjkovich/fsync/Docker/jpredDocker/jpred:/home/docker_user/jpred --user docker_user:10000 -e UID=$(id -u) --name  jpredcontainer2  jpredimage2:latest
+docker run -d -it -v $HOME/jpred-standalone/data:/home/docker_user/mounted/blastdb --user docker_user:10000 -e UID=$(id -u) --name  jpredcontainer2  jpredimage2:latest
 
