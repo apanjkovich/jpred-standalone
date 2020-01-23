9b2e75becbf029c28d74cc24de469454704dabc1
derfredy <derfredy@gmail.com>
2020-01-23 17:05:11 +0100

    Use variables for group and user id

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # add RCE user group and its GID
-sudo /usr/sbin/groupadd linux-sdusers -g 10000
+sudo /usr/sbin/groupadd ${USER_GROUP} -g ${GID}
 
 # Change mounted folder permisions
-sudo chown ${USER}:linux-sdusers $HOME/io
-sudo chown ${USER}:linux-sdusers $HOME/jpred
+sudo chown ${USER}:${GID} ${HOME}/io
+sudo chown ${USER}:${GID} ${HOME}/jpred
 
 # Change UID to real RCE user ID
-sudo usermod -u $UID docker_user
+sudo usermod -u ${UID} ${USER}
 
 # Finally let sudo ask for a passwd
 #sudo sed 's/^/#/' /etc/sudoers.d/docker_user
