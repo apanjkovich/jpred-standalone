5a14485228141ded2ddad1d4f28a15088a25cf1b
derfredy <derfredy@gmail.com>
2020-01-23 17:23:34 +0100

    improve entrypoint comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# add RCE user group and its GID
+# Add user group and its GID inside the container
+# The here is controlling ownership of files produced 
 sudo /usr/sbin/groupadd ${USER_GROUP} -g ${GID}
 
 # Change mounted folder permisions
+# By default thos folders inside the docker container
+# belong to root
 sudo chown ${USER}:${GID} ${HOME}/io
 sudo chown ${USER}:${GID} ${HOME}/jpred
 
-# Change UID to real RCE user ID
+# Change container UID to match host system user ID
 sudo usermod -u ${UID} ${USER}
 
-# Finally let sudo ask for a passwd
+# Finally let sudo ask for a passwd (if needed)
 #sudo sed 's/^/#/' /etc/sudoers.d/docker_user
